4e3ba669360c29699047b016f731355b6862d81f
Gayeon <victoria1559@naver.com>
2017-11-16 19:21:22 +0900

    create project

---
+++ Lecoder/app/src/main/java/com/lecoder/team9/lecoder/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lecoder.team9.lecoder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
