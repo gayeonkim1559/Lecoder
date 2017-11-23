32b3a4b76948c79afa2ba8a3c897593bd40af273
Park JunHyung <schwazodiac@gmail.com>
2017-11-23 18:11:26 +0900

    MainActivity, add fast record list and lecture record list.

--- Lecoder/app/src/main/java/com/lecoder/team9/lecoder/MainActivity.java
+++ Lecoder/app/src/main/java/com/lecoder/team9/lecoder/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton plusBtn, fastRecordBtn, lectureRecordBtn;
     boolean isClickedPlusBtn=false;
     Context mContext;
-    RecyclerView recyclerView;
+    RecyclerView recyclerViewFast,recyclerViewLecture;
     MyAdapter adapter;
-    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.LayoutManager layoutManager,layoutManager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mContext=getApplicationContext();
-        recyclerView= (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
+        recyclerViewFast = (RecyclerView) findViewById(R.id.recyclerView_fast);
+        recyclerViewFast.setHasFixedSize(true);
+
+        recyclerViewLecture= (RecyclerView) findViewById(R.id.recyclerView_lecture);
+        recyclerViewLecture.setHasFixedSize(true);
         //----팝업 버튼 애니메이션 동작
         plusBtn = (FloatingActionButton) findViewById(R.id.mainPlusBtn);
         fastRecordBtn = (FloatingActionButton) findViewById(R.id.mainFastRecordBtn);
@@ -60,28 +64,47 @@ public class MainActivity extends AppCompatActivity {
         });
         //----팝업 버튼 애니메이션 동작
 
-        ArrayList item=new ArrayList<>();
-        item.add(new RecordListItem("느린녹음001","2017/03/14","24:11"));
-        item.add(new RecordListItem("빠른녹음001","2017/08/10","09:31"));
-        item.add(new RecordListItem("빠른녹음002","2017/06/07","00:36"));
+        //----빠른녹음 리스트
+        ArrayList fastItem=new ArrayList<>();
+        fastItem.add(new RecordListItem("느린녹음001","2017/03/14","24:11"));
+        fastItem.add(new RecordListItem("빠른녹음001","2017/08/10","09:31"));
+        fastItem.add(new RecordListItem("빠른녹음002","2017/06/07","00:36"));
 
         layoutManager=new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter=new MyAdapter(item,mContext);
-        recyclerView.setAdapter(adapter);
+        recyclerViewFast.setLayoutManager(layoutManager);
+        adapter=new MyAdapter(fastItem,mContext,false);
+        recyclerViewFast.setAdapter(adapter);
+        //----빠른녹음 리스트
+        ArrayList lectureItem=new ArrayList();
+        lectureItem.add(new RecordListItem("002","2017/11/21","33:12","모바일소프트웨어공학"));
+        lectureItem.add(new RecordListItem("012","2017/06/26","12:55","소프트웨어공학"));
+        lectureItem.add(new RecordListItem("001","2017/10/01","53:04","기초글쓰기"));
+        lectureItem.add(new RecordListItem("002","2017/11/21","33:12","모바일소프트웨어공학"));
+        lectureItem.add(new RecordListItem("012","2017/06/26","12:55","소프트웨어공학"));
+        lectureItem.add(new RecordListItem("012","2017/06/26","12:55","소프트웨어공학"));
+        lectureItem.add(new RecordListItem("012","2017/06/26","12:55","소프트웨어공학"));
+        lectureItem.add(new RecordListItem("001","2017/10/01","53:04","끝"));
+
+        layoutManager2=new LinearLayoutManager(this);
+        recyclerViewLecture.setLayoutManager(layoutManager2);
+        adapter=new MyAdapter(lectureItem,mContext,true);
+        recyclerViewLecture.setAdapter(adapter);
+
     }
     class MyAdapter extends RecyclerView.Adapter{
         private Context context;
         private ArrayList<RecordListItem> mItems;
-
-        public MyAdapter(ArrayList mItems,Context context) {
+        private boolean isLectureList;
+        private View v;
+        public MyAdapter(ArrayList mItems,Context context,boolean isLectureList) {
             this.mItems = mItems;
             this.context = context;
+            this.isLectureList =isLectureList;
         }
 
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v= LayoutInflater.from(parent.getContext()).inflate(R.layout.fastrecord_list,parent,false);
+            v= LayoutInflater.from(parent.getContext()).inflate(R.layout.record_list,parent,false);
             MyViewHolder holder=new MyViewHolder(v);
             return holder;
         }
@@ -93,6 +116,13 @@ public class MainActivity extends AppCompatActivity {
             aHolder.recordName.setText(mItems.get(position).recordName);
             aHolder.recordDuration.setText(mItems.get(position).recordDuration);
             aHolder.recordDate.setText(mItems.get(position).recordDate);
+            if (isLectureList){
+                aHolder.recordClass.setText(mItems.get(position).recordClass);
+                aHolder.recordClass.setVisibility(View.VISIBLE);
+                TextView recordClassText= v.findViewById(R.id.recordName);
+                RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) recordClassText.getLayoutParams();
+                params.addRule(RelativeLayout.LEFT_OF, R.id.recordGoBtn);
+            }
         }
 
         @Override
@@ -104,9 +134,12 @@ public class MainActivity extends AppCompatActivity {
             public TextView recordDate,recordName,recordDuration,recordClass;
             public MyViewHolder(View itemView) {
                 super(itemView);
-                recordDate =itemView.findViewById(R.id.fastRecordDate);
-                recordDuration=itemView.findViewById(R.id.fastRecordDuration);
-                recordName=itemView.findViewById(R.id.fastRecordName);
+                recordDate =itemView.findViewById(R.id.recordDate);
+                recordDuration=itemView.findViewById(R.id.recordDuration);
+                recordName=itemView.findViewById(R.id.recordName);
+                if (isLectureList){
+                    recordClass=itemView.findViewById(R.id.recordClass);
+                }
             }
         }
     }
