d48777f205db936418e3144e195fd83e77cea4ba
Gayeon <victoria1559@naver.com>
2017-11-23 19:04:25 +0900

    create recordActivity

---
+++ Lecoder/app/src/main/java/com/lecoder/team9/lecoder/RecordActivity.java
@@ -0,0 +1,17 @@
+package com.lecoder.team9.lecoder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by GAYEON on 2017-11-23.
+ */
+
+public class RecordActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record);
+    }
+}
