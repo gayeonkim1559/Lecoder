7c61f5ec3a0c282664d16da83bd3f5c2ca197986
Gayeon <victoria1559@naver.com>
2017-11-23 20:17:21 +0900

    add fragments to recordActivity

--- Lecoder/app/src/main/java/com/lecoder/team9/lecoder/RecordActivity.java
+++ Lecoder/app/src/main/java/com/lecoder/team9/lecoder/RecordActivity.java
@@ -3,6 +3,7 @@ package com.lecoder.team9.lecoder;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 
 /**
  * Created by GAYEON on 2017-11-23.
@@ -13,5 +14,7 @@ public class RecordActivity extends Activity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
+
+
     }
 }
