0f6eb9bd9b70f16164b397bcc81febc3509409eb
Gayeon <victoria1559@naver.com>
2017-11-27 14:41:26 +0900

    record activity that contains memo animation

--- Lecoder/app/src/main/java/com/lecoder/team9/lecoder/RecordActivity.java
+++ Lecoder/app/src/main/java/com/lecoder/team9/lecoder/RecordActivity.java
@@ -27,6 +27,12 @@ public class RecordActivity extends FragmentActivity implements View.OnClickList
     Fragment memoFragment = new MemoFragment();
     Fragment drawingFragment = new MemoFragment();
 
+    private final int OPEN = 1;
+    private final int CLOSED = 2;
+
+    private int memoFragment_Flag = CLOSED;
+    private int drawingFragment_Flag = CLOSED;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,30 +48,62 @@ public class RecordActivity extends FragmentActivity implements View.OnClickList
         memoBtn.setOnClickListener(this);
         drawingBtn.setOnClickListener(this);
         cameraBtn.setOnClickListener(this);
-
-        //slideUpAnimation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fragment_slide_up);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
                 case R.id.memoBtn:
-                    callFragment(MEMO);
+                    if (memoFragment_Flag == CLOSED) {
+                        openFragment(MEMO);
+                        memoFragment_Flag = OPEN;
+                    }
+                    else {
+                        closeFragment(MEMO);
+                        memoFragment_Flag = CLOSED;
+                    }
                     break;
                 case R.id.drawingBtn:
-                    callFragment(DRAWING);
+                    if (drawingFragment_Flag == CLOSED) {
+                        openFragment(DRAWING);
+                        drawingFragment_Flag = OPEN;
+                    }
+                    else {
+                        closeFragment(DRAWING);
+                        drawingFragment_Flag = CLOSED;
+                    }
                     break;
                 case R.id.cameraBtn:
-            }
+        }
     }
 
-    private void callFragment(int fragmentNum) {
+    private void openFragment(int fragmentNum) {
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
         switch (fragmentNum) {
             case MEMO:
                 transaction.setCustomAnimations(R.anim.slide_in_up, R.anim.slide_out_up);
+                transaction.replace(R.id.fragment_container, memoFragment);
+                transaction.commit();
+                break;
+
+            case DRAWING:
+                transaction.replace(R.id.fragment_container, drawingFragment);
+                //transaction.setCustomAnimations(R.anim.fragment_slide_up, R.anim.fragment_slide_down);
+                transaction.commit();
+                break;
+        }
+    }
+
+    private void closeFragment(int fragmentNum) {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        switch (fragmentNum) {
+            case MEMO:
+                //transaction.setCustomAnimations(R.anim.slide_in_down, R.anim.slide_out_down);
+                //transaction.setCustomAnimations()
                 transaction.replace(R.id.fragment_container, memoFragment);
                 transaction.commit();
                 break;
